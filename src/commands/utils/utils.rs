2468f5991f5b4664cfe28c185579c8bec2a64177
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2025-06-13 12:58:52 +0200

    Handle user interactions with voice channels selection menu

--- src/commands/utils/utils.rs
+++ src/commands/utils/utils.rs
@@ -62,7 +62,9 @@ pub async fn select_voice_channels_menu(
         return Ok(());
     }
 
-    let menu: CreateSelectMenu = CreateSelectMenu::new("Select voice channels", CreateSelectMenuKind::String { options: options } )
+    let select_menu_custom_id = "Select voice channels".to_string();
+
+    let menu: CreateSelectMenu = CreateSelectMenu::new(select_menu_custom_id.clone(), CreateSelectMenuKind::String { options: options } )
         .placeholder("Select voice channels")
         .min_values(2)
         .max_values(options_len);
@@ -70,6 +72,60 @@ pub async fn select_voice_channels_menu(
     ctx.send(poise::CreateReply::default()
         .components(vec![CreateActionRow::SelectMenu(menu)])
     ).await?;
+
+
+    let interaction: Option<ComponentInteraction> = {
+        let author_id = ctx.author().id;
+        let select_menu_custom_id = select_menu_custom_id.clone();
+
+        ComponentInteractionCollector::new(ctx.serenity_context().shard.clone())
+            .custom_ids(vec![select_menu_custom_id])
+            .author_id(author_id)
+            .timeout(std::time::Duration::from_secs(60))
+            .await
+    };
+
+    if let Some(interaction) = interaction {
+        interaction.create_response(ctx.serenity_context(),
+        CreateInteractionResponse::UpdateMessage(CreateInteractionResponseMessage::default()))
+        .await?;
+
+        let selected_channel_ids: Vec<poise::serenity_prelude::ChannelId> = match &interaction.data.kind {
+            poise::serenity_prelude::ComponentInteractionDataKind::StringSelect{values} => {
+                values.iter()
+                    .filter_map(|id_str| id_str.parse::<u64>().ok())
+                    .map(ChannelId::new)
+                    .collect()
+            }
+            _ => {
+                ctx.say("Unexpected component interaction type.").await?;
+                return Ok(());
+            }
+        };
+
+
+        let channel_names: Vec<String> = {
+            let guild = ctx
+                .cache()
+                .ok_or("Cache unavailable")?
+                .guild(guild_id)
+                .ok_or("Guild not in cache")?;
+
+            selected_channel_ids
+                .iter()
+                .filter_map(|cid| guild.channels.get(cid))
+                .map(|ch| ch.name.clone())
+                .collect()
+        };
+
+        ctx.say(format!(
+            "You selected: {}",
+            channel_names.join(", ")
+        )).await?;
+    }
+    else {
+        ctx.say("Timeout reached without selection!!!").await?;
+    }
 
     Ok(())
 }
